a379f34dbf22bf95447b49698abcb6ac3cc467fe
Rob Myers <Rob.Myers@agileInstitute.com>
2025-05-28 19:40:40 -0700

    minor change

--- LegacyTesting/LegacyTesting/OutputProxy.cs
+++ LegacyTesting/LegacyTesting/OutputProxy.cs
@@ -2,21 +2,21 @@ namespace LegacyTesting;
 
 public class OutputProxy
 {
-    private readonly TextWriter originalTextWriter;
+    private readonly TextWriter _originalTextWriter;
 
     public OutputProxy(TextWriter original)
     {
-        originalTextWriter = original;
+        _originalTextWriter = original;
     }
 
     public virtual void WriteLine(string s)
     {
-        originalTextWriter.WriteLine(s);
+        _originalTextWriter.WriteLine(s);
     }
 
     public virtual void WriteLine(
         string format, params object?[] arguments)
     {
-        originalTextWriter.WriteLine(format, arguments);
+        _originalTextWriter.WriteLine(format, arguments);
     }
 }
